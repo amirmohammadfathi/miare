e4153288063381741cf089c4febeed3a82b019ba
amir mohammad fathi <amir.mohammad.fathi.2002@gmail.com>
2024-01-05 17:29:37 +0330

    FEAT: define trip, additionalEarning,DailyEarning and WeeklyEarning

--- couriers/models.py
+++ couriers/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.core.validators import MinValueValidator
+from datetime import date
 import uuid
 
 
@@ -17,7 +19,67 @@ class Courier(AbstractBaseUser):
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
 
-# Create your models here.
     def __str__(self):
         return str(self.id)
 
+
+class Trip(models.Model):
+    id = models.UUIDField(auto_created=True, primary_key=True, default=uuid.uuid4, editable=False)
+    courier = models.ForeignKey(Courier, related_name='courier', on_delete=models.CASCADE)
+
+    income = models.FloatField(default=0.0, validators=[MinValueValidator(0.0)])
+    date = models.DateField(default=date.today())
+    distance = models.FloatField(null=True, blank=True)
+    customer_type = models.CharField(max_length=150, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class AdditionalEarning(models.Model):
+    id = models.UUIDField(auto_created=True, primary_key=True, default=uuid.uuid4, editable=False)
+    courier = models.ForeignKey(Courier, related_name='courier_additional_earning', on_delete=models.CASCADE)
+    amount = models.FloatField(null=True, blank=True)
+    reason = models.TextField(null=True, blank=True)
+    date = models.DateField(default=date.today())
+    is_for_award = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class DailyEarning(models.Model):
+    id = models.UUIDField(auto_created=True, primary_key=True, default=uuid.uuid4, editable=False)
+    courier = models.ForeignKey(Courier, related_name='courier_daily_earning', on_delete=models.CASCADE)
+    date = models.DateField(default=date.today(), editable=False)
+    earnings = models.FloatField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class WeeklyEarning(models.Model):
+    id = models.UUIDField(auto_created=True, primary_key=True, default=uuid.uuid4, editable=False)
+    courier = models.ForeignKey(Courier, related_name='courier_weekly_earning', on_delete=models.CASCADE)
+    from_date = models.DateField()
+    to_date = models.DateField()
+    earnings = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.id)
