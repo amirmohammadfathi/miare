865d265e80664fa8658f1e4021b89bec2d1551c6
amir mohammad fathi <amir.mohammad.fathi.2002@gmail.com>
2024-01-05 17:17:56 +0330

    FIX: fix bug of not recognize .env variables

--- core/environments.py
+++ core/environments.py
@@ -1,5 +1,10 @@
 from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DEBUG = bool(int(environ.get('DEBUG', 0)))
 SECRET_KEY = environ.get('SECRET_KEY')
 ALLOWED_HOSTS = environ.get('ALLOWED_HOSTS', ' ').split()
+
+
