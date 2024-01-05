4c39352eeb4da5f8441ec5dd97c72c9a252c8d1b
amir mohammad fathi <amir.mohammad.fathi.2002@gmail.com>
2024-01-05 17:44:33 +0330

    FEAT: view for retrieve daily and weekly earning of couriers

--- couriers/views.py
+++ couriers/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from rest_framework.generics import RetrieveAPIView, CreateAPIView, ListAPIView
+
+from couriers.serializers import (
+    CourierSerializer, TripSerializer, AdditionalEarningSerializer, WeeklyEarningSerializer, DailyEarningSerializer
+)
+from couriers.models import Courier, AdditionalEarning, DailyEarning, WeeklyEarning
+from datetime import date, timedelta
+
+
 class CreateListCouriers(CreateAPIView, ListAPIView):
     serializer_class = CourierSerializer
     queryset = Courier.objects.all()
@@ -11,7 +19,6 @@ class TripOfCourier(CreateAPIView):
         serializer.save(courier_id=self.kwargs.get("pk"))
 
 
-# Create your views here.
 class AdditionalEarningOfCourier(CreateAPIView, ListAPIView):
     serializer_class = AdditionalEarningSerializer
 
@@ -22,4 +29,28 @@ class AdditionalEarningOfCourier(CreateAPIView, ListAPIView):
         return (
             AdditionalEarning.objects.filter(date=date.today(), courier_id=self.kwargs.get("pk"))
             .select_related('courier').all()
+        )
+
+
+class RetrieveCourierDailyEarning(RetrieveAPIView):
+    serializer_class = DailyEarningSerializer
+
+    def get_object(self):
+        return (
+            DailyEarning.objects.filter(date=date.today(), courier_id=self.kwargs.get("pk"))
+            .select_related('courier').first()
+        )
+
+
+class RetrieveCourierWeeklyEarning(RetrieveAPIView):
+    serializer_class = WeeklyEarningSerializer
+
+    def get_object(self):
+        today = date.today()  # Saturday
+        last_weak_saturday = today - timedelta(days=7)
+        last_weak_friday = today - timedelta(days=1)
+        return (
+            WeeklyEarning.objects.filter(
+                from_date=last_weak_saturday, to_date=last_weak_friday, courier_id=self.kwargs.get("pk")
+            ).select_related('courier').prefetch_related('courier__courier_daily_earning').first()
         )
