4b79b0c56f3eb379dc333ed96bd8cc514eda8442
amir mohammad fathi <amir.mohammad.fathi.2002@gmail.com>
2024-01-05 17:27:32 +0330

    FEAT: define couriers table

--- couriers/models.py
+++ couriers/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+import uuid
+
+
+class Courier(AbstractBaseUser):
+    id = models.UUIDField(auto_created=True, primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    password = None
+    last_login = None
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_deleted = models.BooleanField(default=False)
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
 # Create your models here.
+    def __str__(self):
+        return str(self.id)
+
