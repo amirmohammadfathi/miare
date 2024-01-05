a26da98032fd6c34c112cf6372d67d324a2ee204
amir mohammad fathi <amir.mohammad.fathi.2002@gmail.com>
2024-01-05 17:41:34 +0330

    FEAT: view for create trip for couriers

--- couriers/views.py
+++ couriers/views.py
@@ -3,4 +3,12 @@ class CreateListCouriers(CreateAPIView, ListAPIView):
     serializer_class = CourierSerializer
     queryset = Courier.objects.all()
 
+
+class TripOfCourier(CreateAPIView):
+    serializer_class = TripSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(courier_id=self.kwargs.get("pk"))
+
+
 # Create your views here.
