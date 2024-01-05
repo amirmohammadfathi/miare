47b8f400a431c49e2e88b0ee245a780c93157fb7
amir mohammad fathi <amir.mohammad.fathi.2002@gmail.com>
2024-01-05 17:40:22 +0330

    FEAT: view for create and retrieve all couriers

--- couriers/views.py
+++ couriers/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+class CreateListCouriers(CreateAPIView, ListAPIView):
+    serializer_class = CourierSerializer
+    queryset = Courier.objects.all()
 
 # Create your views here.
